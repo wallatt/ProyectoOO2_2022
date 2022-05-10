1a35736e56e7d49ccc44b7cf8f4a299f50f4bbce
alanlo97 <ahlan9673@gmail.com>
2022-05-10 13:54:42 -0300

    Proyecto vacio

---
+++ TrabajoPractico/src/main/java/com/unla/grupo13/TrabajoPractico/TrabajoPracticoApplication.java
@@ -0,0 +1,13 @@
+package com.unla.grupo13.TrabajoPractico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoPracticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoPracticoApplication.class, args);
+	}
+
+}
---
+++ TrabajoPractico/src/test/java/com/unla/grupo13/TrabajoPractico/TrabajoPracticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.grupo13.TrabajoPractico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoPracticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
