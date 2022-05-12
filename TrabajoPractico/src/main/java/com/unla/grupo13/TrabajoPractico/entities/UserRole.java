174d7333bdf8ca55590e0f525b9375ee56743675
FedeOA <fedeacosta07@hotmail.com>
2022-05-11 21:13:37 -0300

    UserRole class

--- TrabajoPractico/src/main/java/com/unla/grupo13/TrabajoPractico/entities/UserRole.java
+++ TrabajoPractico/src/main/java/com/unla/grupo13/TrabajoPractico/entities/UserRole.java
@@ -2,4 +2,50 @@ package com.unla.grupo13.TrabajoPractico.entities;
 
 public class UserRole {
 
+	
+	private int id;
+	private String role;
+	private boolean enabled;
+	
+	public UserRole () {}
+	
+	
+	public UserRole(String role, boolean enabled) {
+		
+		this.role=role;
+		this.enabled=enabled;
+		
+		
+		
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
 }
