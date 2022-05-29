314869a20858be0d70cdeefc1494b935f9e6866d
Bautista Berto <bertobautista03@gmail.com>
2022-05-29 00:50:49 -0300

    Se agregaron vistas , se modifico la ruta home y se soluciono error en la clase Tradicional agregando getters and setters

--- TrabajoPractico/src/main/java/com/unla/grupo13/TrabajoPractico/controllers/HomeController.java
+++ TrabajoPractico/src/main/java/com/unla/grupo13/TrabajoPractico/controllers/HomeController.java
@@ -7,18 +7,23 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.unla.grupo13.TrabajoPractico.helpers.ViewRouteHelper;
 import com.unla.grupo13.TrabajoPractico.services.INotaPedidoService;
 
 @Controller
-@RequestMapping
-public class HelloWorldController {
+@RequestMapping("/")
+public class HomeController {
 
 	
 	
 
-    @GetMapping("/helloworld")
-    public String helloWorld(){
-        return "index";
+    @GetMapping("home")
+    public ModelAndView home(){
+    	ModelAndView mAV = new ModelAndView(ViewRouteHelper.HOME);
+    	
+    	return mAV;
     }
     
     
