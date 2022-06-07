dddc1f3bb06a193d18c7a5676b2e8e09e193060e
FedeOA <fedeacosta07@hotmail.com>
2022-06-07 00:56:15 -0300

    re ruteo generarEspacios

--- TrabajoPractico/src/main/java/com/unla/grupo13/TrabajoPractico/entities/Aula.java
+++ TrabajoPractico/src/main/java/com/unla/grupo13/TrabajoPractico/entities/Aula.java
@@ -1,45 +1,58 @@
 package com.unla.grupo13.TrabajoPractico.entities;
 
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.*;
 
-
 @Entity
+	@Inheritance(strategy = InheritanceType.JOINED)
+	public abstract class Aula extends EntityBase{
 
-@Inheritance(strategy = InheritanceType.JOINED)
-public abstract class Aula extends EntityBase{
+	   
+		@Column(name = "numero")
+	    protected int numero;
 
-   
-	@Column(name = "numero")
-    protected int numero;
+	    @ManyToOne(fetch = FetchType.LAZY)
+	    @JoinColumn(name = "edificio_id", nullable = false)
+	    protected Edificio edificio;
+	   
+	    @OneToMany(fetch=FetchType.LAZY, mappedBy="aula")
+		protected Set<Espacio> espacios=new HashSet<Espacio>();
+		
+	    
+	    public Set<Espacio> getEspacios() {
+			return espacios;
+		}
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "edificio_id", nullable = false)
-    protected Edificio edificio;
 
-    
-    public Aula() {}
+		public void setEspacios(Set<Espacio> espacios) {
+			this.espacios = espacios;
+		}
 
 
-	public int getNumero() {
-		return numero;
+		public Aula() {}
+
+
+		public int getNumero() {
+			return numero;
+		}
+
+
+		public void setNumero(int numero) {
+			this.numero = numero;
+		}
+
+
+		public Edificio getEdificio() {
+			return edificio;
+		}
+
+
+		public void setEdificio(Edificio edificio) {
+			this.edificio = edificio;
+		}
+	    
+	    
+	    
 	}
-
-
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
-
-
-	public Edificio getEdificio() {
-		return edificio;
-	}
-
-
-	public void setEdificio(Edificio edificio) {
-		this.edificio = edificio;
-	}
-    
-    
-    
-}
--- TrabajoPractico/src/main/java/com/unla/grupo13/TrabajoPractico/entities/Espacio.java
+++ TrabajoPractico/src/main/java/com/unla/grupo13/TrabajoPractico/entities/Espacio.java
@@ -3,64 +3,15 @@ package com.unla.grupo13.TrabajoPractico.entities;
 import java.time.LocalDate;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-
 public class Espacio extends EntityBase {
 	
-	public LocalDate getFecha() {
-		return fecha;
-	}
-
-	public void setFecha(LocalDate fecha) {
-		this.fecha = fecha;
-	}
-
-	public char getTurno() {
-		return turno;
-	}
-
-	public void setTurno(char turno) {
-		this.turno = turno;
-	}
-
-	public Aula getAula() {
-		return aula;
-	}
-
-	public void setAula(Aula aula) {
-		this.aula = aula;
-	}
-
-	public boolean isLibre() {
-		return libre;
-	}
-
-	public void setLibre(boolean libre) {
-		this.libre = libre;
-	}
-
-	public NotaPedido getNotaPedido() {
-		return notaPedido;
-	}
-
-	public void setNotaPedido(NotaPedido notaPedido) {
-		this.notaPedido = notaPedido;
-	}
-
-	public Espacio(LocalDate fecha2, char turno2, Aula aula2, boolean libre2) {
-		// TODO Auto-generated constructor stub
-	}
-
 	@Column(name="fecha")
 	private LocalDate fecha;
 	
@@ -77,9 +28,72 @@ public class Espacio extends EntityBase {
 	@ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "notapedido_id", nullable = true)
 	private NotaPedido notaPedido;
-	
 
-	
-	
-	
+
+	public Espacio() {}
+    public Espacio(Aula aula, char turno, LocalDate fecha, boolean libre) {
+    }
+
+
+
+
+
+	public Espacio(LocalDate fecha2, char turno2, Aula aula2, boolean libre2) {
+				this.fecha=fecha2;
+				this.aula=aula2;
+				this.turno=turno2;
+				this.libre=libre2;
+	}
+
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+
+	public char getTurno() {
+		return turno;
+	}
+
+
+	public void setTurno(char turno) {
+		this.turno = turno;
+	}
+
+
+	public Aula getAula() {
+		return aula;
+	}
+
+
+	public void setAula(Aula aula) {
+		this.aula = aula;
+	}
+
+
+	public boolean isLibre() {
+		return libre;
+	}
+
+
+	public void setLibre(boolean libre) {
+		this.libre = libre;
+	}
+
+
+	public NotaPedido getNotaPedido() {
+		return notaPedido;
+	}
+
+
+	public void setNotaPedido(NotaPedido notaPedido) {
+		this.notaPedido = notaPedido;
+	}
+    
+    
 }
